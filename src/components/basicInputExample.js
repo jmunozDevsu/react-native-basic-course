60f918bfea6bc22b9438a41319e38b70e6dfc4d9
Javier Muñoz <jmunoz@devsu.com>
2020-03-02 16:23:20 -0500

    added state handling

--- src/components/basicInputExample.js
+++ src/components/basicInputExample.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, TextInput, View } from 'react-native';
 import strings from '../res/strings';
 
-function BasicInputExample() {
-  return (
-    <View>
-      <Text>
-        {strings.textInput.label}
-      </Text>
-      <TextInput
-        placeholder={strings.textInput.hint}
-      />
-    </View>
-  );
+class BasicInputExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          {strings.textInput.label}
+        </Text>
+        <TextInput
+          value={this.state.value}
+          onChangeText={(value) => this.setState({ value })}
+          placeholder={strings.textInput.hint}
+        />
+        <Text>
+          {this.state.value}
+        </Text>
+      </View>
+    );
+  }
 }
 
 export default BasicInputExample;
