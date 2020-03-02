42f9c8ffe49ad6c7199a0db66bbb94b0f89f0374
Javier Muñoz <jmunoz@devsu.com>
2020-03-02 16:51:45 -0500

    different sources

--- src/components/imageExample.js
+++ src/components/imageExample.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 
 function ImageExample() {
   return (
-    <Image
-      source={require('../res/images/os.jpeg')}
-    />
+    <View>
+      <Image
+        source={require('../res/images/thumbsUp.jpeg')}
+      />
+      <Image
+        style={{width: 50, height: 50}}
+        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+      />
+      <Image
+        style={{width: 66, height: 58}}
+        source={{ uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==' }}
+      />
+    </View>
   );
 }
 
