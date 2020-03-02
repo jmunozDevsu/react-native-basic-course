8a6d2db7a2d840f05ba37b0abe21513142974c5d
Javier Muñoz <jmunoz@devsu.com>
2020-03-02 15:50:32 -0500

    rebased

--- src/helloClassComponent.js
+++ src/helloClassComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 class HelloClassComponent extends Component {
   render() {
