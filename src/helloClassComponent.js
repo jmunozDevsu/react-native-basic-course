5068501728afb057a0ef9f2afca46e0cf6ee7f20
Javier Muñoz <jmunoz@devsu.com>
2020-03-02 15:01:58 -0500

    added styles

--- src/helloClassComponent.js
+++ src/helloClassComponent.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 class HelloClassComponent extends Component {
   render() {
     return (
-      <View>
-        <Text>{'Hello Class Component'}</Text>
+      <View
+        style={styles.container}
+      >
+        <Text
+          style={styles.blueText}
+        >
+          {'Hello '}
+          <Text
+            style={styles.boldText}
+          >
+            {'Class Component'}
+          </Text>
+        </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  blueText: {
+    color: 'blue',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
 
 export default HelloClassComponent;
--- src/helloFunctionComponent.js
+++ src/helloFunctionComponent.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 function HelloFunctionComponent() {
   return (
-    <View>
-      <Text>{'Hello Function Component'}</Text>
+    <View style={styles.container}>
+      <Text
+        style={[styles.blueText, styles.boldText]}
+      >
+        {'Hello '}
+      </Text>
+      <Text
+        style={[styles.redText, styles.boldText]}
+      >
+        {'Functional Component'}
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  redText: {
+    color: 'red',
+  },
+  blueText: {
+    color: 'blue',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
 
 export default HelloFunctionComponent;
