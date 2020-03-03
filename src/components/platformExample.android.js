a6bf6c29fa2e5d0f872e6d87066c8417a24ef26a
Javier Muñoz <jmunoz@devsu.com>
2020-03-03 17:18:34 -0500

    platform specific file example

---
+++ src/components/platformExample.android.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function PlatformExample() {
+
+  return (
+    <Text>
+      {'android file'}
+    </Text>
+  );
+}
+
+export default PlatformExample;
---
+++ src/components/platformExample.ios.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-native';
+
+function PlatformExample() {
+
+  return (
+    <Button
+      title={'ios file'}
+    />
+  );
+}
+
+export default PlatformExample;
