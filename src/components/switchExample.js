9945bf3529d67753400340c9251c8b0e14255178
Javier Muñoz <jmunoz@devsu.com>
2020-03-03 15:40:18 -0500

    handle switch data

--- src/components/switchExample.js
+++ src/components/switchExample.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Switch } from 'react-native';
 
 function SwitchExample() {
+  const [value, setValue] = React.useState(false);
   return (
     <Switch
+      value={value}
+      onValueChange={setValue}
     />
   );
 }
